d65b4c77e246e1611e5ca869fe344ef329340b0d
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-02-25 08:48:30 +0300

    first commit

---
+++ src/kot.kt
@@ -0,0 +1,85 @@
+fun main(){
+    var note = "hello"
+    var name = "liz"
+    var sum = "hello"+"liz"
+    println("hello"+ " "+"liz")
+    one()
+    two()
+three()
+    four()
+    five()
+    six()
+    seven()
+}
+fun sum(hello:String,liz:String):String{
+    var sum =("fact"+"liz")
+    return sum
+}
+fun one(){
+   var x= remainder(90,19)
+    println(x)
+
+}
+fun remainder(number1:Int,number2:Int):Int{
+    var modulus=number1 % number2
+    return modulus
+}
+
+fun two(){
+    var al=l(50,3)
+    println(al)
+
+}
+fun l(x:Int,y:Int):Int{
+    var modulus =x%y
+            return modulus
+}
+
+fun three (){
+    var u=add(30, 40)
+    println(u)
+}
+fun add(y:Int,h:Int):Int{
+    var sum=y+h
+    return sum
+
+}
+
+fun four(){
+  var name=" liz"
+  var tribe="bakiiga"
+    var both= "liz "+" is a" +"bakiiga"
+    println(both)
+}
+fun tribe(liz:String,bakiiga:String ):String{
+     var tribe=liz+bakiiga
+     return tribe
+}
+fun five(){
+ var peep= product(400,30)
+  var product= 400*30
+
+    println(peep)
+}
+fun product(m:Int,n:Int):Int{
+    var product=m*n
+    return product
+}
+fun six(){
+   var me = difference(300,255)
+    var difference =(300-255)
+    println(me)
+}
+fun difference(s:Int,f:Int):Int{
+    var difference = s-f
+    return difference
+}
+fun seven (){
+    var lee = division(10000,50)
+    var divide =10000/50
+    println(lee)
+}
+fun division(g:Int,h:Int):Int{
+    var division =g/h
+   return division
+}
